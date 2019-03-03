36970e95214d09b2bce490faebdb65eb04434b7c
Averchenkov Nikita IKBO-02-17 <31746573+Averchenkov@users.noreply.github.com>
2019-03-03 14:46:37 +0300

    test commit

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Aver on 03.03.2019.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by Aver on 03.03.2019.
+//
+
+#ifndef TEST2_MAIN_H
+#define TEST2_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //TEST2_MAIN_H
